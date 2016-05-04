f11d2093a7719bbc36da31ea45423e7237170436
Mary Nyamor <mnyamor@gmail.com>
2016-05-04 23:12:14 +0300

    adding express session and cookie parser

--- server.js
+++ server.js
@@ -9,9 +9,20 @@ var port = process.env.PORT || 8080;
 //HTTP request logger middleware for node.js
 var morgan = require('morgan');
 
+var cookieParser = require('cookie-parser'); 
+var session = require('express-session'); 
+
+
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({ secret:'anystringoftext', saveUninitialized: true, resave: true }));
+
 app.use('/', function (req , res) {
     res.send('first express program');
+    console.log(req.cookies);
+    console.log('=============== awesomeness ================= ');
+    console.log(req.session);
+
 });
 app.listen(port);
 console.log('server running on port ' + port);
